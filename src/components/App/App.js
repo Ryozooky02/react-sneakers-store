9e567b0cf9f979265da6c68fd4e220bc9699e078
Ordabek Adil <adilordabekmen@gmail.com>
2023-08-02 23:16:55 +0600

    FunctionalSpace

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,43 @@
 import '../App/App.scss';
 import Header from '../Header/Header';
-import Cards from '../Cards/Cards';
+import Card from '../Card/Card';
 import Drawer from '../Drawer/Drawer';
+import Search from '../../resources/svg/search-card.svg';
+import { useEffect, useState } from 'react';
+
 
 
 function App() {
+
+  const [items, setItems] = useState([])
+  const [CartOpened, setCartOpened] = useState(false)
+
+  useEffect(() => {
+    fetch('https://64ca5b0f700d50e3c704c5cc.mockapi.io/items')
+  .then(res => res.json())
+  .then(json => setItems(json))
+  }, [])
+
   return (
     <div className="App">
       <div className="wrapper">
-        <Header/>
-        <Cards/>
-        <Drawer/>
+        {CartOpened && <Drawer DataCard={items} onCloseDrawer={() => setCartOpened(false)}/>}
+        <Header onOpenCart={() => setCartOpened(true)}/>
+
+        <div className="Cards__header">
+                <div className="Cards__title">
+                    <h1>Все кроссовки</h1>
+                </div>
+                <div className="Cards__input">
+                    <img src={Search} alt="search" />
+                    <input type="text" placeholder="Поиск..."/>
+                </div>
+            </div>
+        <div className="Cards_hero">
+          {items.map((item, index) => (
+              <Card key={index} name={item.name} price={item.price} image={item.image} onCheck={() => console.log(item)}/>
+            ))}
+        </div>
       </div>
     </div>
   );
