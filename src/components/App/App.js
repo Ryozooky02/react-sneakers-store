15563f75239cb82f3b51a91cf455df3b7286bce8
Ordabek Adil <adilordabekmen@gmail.com>
2023-08-04 00:11:31 +0600

    FilterCardCommit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,12 +12,15 @@ function App() {
   const [items, setItems] = useState([]);
   const [CartOpened, setCartOpened] = useState(false);
   const [CartItems, setCartItems] = useState([]);
+  const [SearchValue, setSearchValue] = useState('')
 
   const onAddtoCart = (obj) => {
     setCartItems([...CartItems, obj])
   }
 
-  console.log(CartItems);
+  const onSearchCard = (e) => {
+    setSearchValue(e.target.value)
+  }
 
   useEffect(() => {
     fetch('https://64ca5b0f700d50e3c704c5cc.mockapi.io/items')
@@ -33,15 +36,19 @@ function App() {
 
         <div className="Cards__header">
                 <div className="Cards__title">
-                    <h1>Все кроссовки</h1>
+                    <h1>{SearchValue ? `Поиск по запросу '${SearchValue}'` : 'Все кросовки'}</h1>
                 </div>
                 <div className="Cards__input">
                     <img src={Search} alt="search" />
-                    <input type="text" placeholder="Поиск..."/>
+                    <input onChange={onSearchCard} value={SearchValue} type="text" placeholder="Поиск..."/>
+                    { SearchValue && <button onClick={() => setSearchValue('')} className='clearInput'><svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="white" stroke="#DBDBDB"/>
+                                <path d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z" fill="#B5B5B5"/>
+                                </svg></button>}
                 </div>
             </div>
         <div className="Cards_hero">
-          {items.map((item, index) => (
+          {items.filter(item => item.name.toLowerCase().includes(SearchValue.toLowerCase())).map((item, index) => (
               <Card key={index} name={item.name} price={item.price} image={item.image} onPlus={(obj) => onAddtoCart(obj)}/>
             ))}
         </div>
