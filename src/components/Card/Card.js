1532b5b8c8d11b3edf172473d8697e32fb8bcdb9
Ordabek Adil <adilordabekmen@gmail.com>
2023-08-11 12:24:26 +0600

    ComminInCard

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,10 +6,10 @@ const Cards = ({image, name, price, onPlus}) => {
 
 
     const [isAdded, setIsAdded] = useState(false);
-    const [isLiked, setIsLiked] = useState(false);
+    const [isFavorite, setIsFavorite] = useState(false);
     
     const onClickLikeDislike = () => {
-        setIsLiked(!isLiked);
+        setIsFavorite(!isFavorite);
     }
     
     const onClickPlus = () => {
@@ -28,7 +28,7 @@ const Cards = ({image, name, price, onPlus}) => {
                 <div className="Card">
                             <div className="Card__img">
                                 <img src={image} alt="Card" />
-                                <div className={ isLiked ? 'Card__favorite-Active' : 'Card__favorite-nonActive'}>
+                                <div className={ isFavorite ? 'Card__favorite-Active' : 'Card__favorite-nonActive'}>
                                     <svg onClick={onClickLikeDislike} width={20}  height={20} clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fill-rule="nonzero"/></svg>
                                     </div>
                             </div>
